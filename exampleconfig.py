c46d79882ad51609c2436fe0aed039145a9b8aed
biilal1 <135733490+biilal1@users.noreply.github.com>
2024-08-13 17:58:20 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,16 +1,16 @@
 from sample_config import Config
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = خلي ايبي ايدي هنا
-    API_HASH = "الايبي هاش"
+    APP_ID = 9671629
+    API_HASH = "be5c84e9dc1ca0e2b53d54b71e575124"
     # the name to display in your alive message
-    ALIVE_NAME = "اسمك ع التليجرام"
+    ALIVE_NAME = "BiLaL"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "رابط قاعدة البيانات"
+    DB_URI = "postgresql://postgres:oakwox3@localhost:5432/owkwkso"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
-    STRING_SESSION = "كود التيرمكس"
+    STRING_SESSION = "1ApWapzMBuwyjrOqC1M_uEykYOuaCul77fEK-VKjcllKSDY70kVI2YV_K0xHlKdc2DWEWsJk51IT43QjjSWq12dHgC7xDiDH_VIZrIKM3pNOVWBnH5xiHvmxASVlz4xlLtGPI28BlZ4m6mzoqt8vGw52LqgZedeVvSNZxEVVtypNrtM9Sv4WGL7GnYJRQgxJ1JtKT_0EZdX91Iyc2Lj5aWYImdqtkCJraulQDJTJYLNSpfukUAnMkEGB5KlMVN8bfsMggvqDUFLkHX9iQDoSL9FfB_SeWzTnRSkNZHZc7TvuSyq_8VokWo9bdR_UGWZgReo3_yR6jpeah4mmjrrnjvaBgqQWhOc4="
     # create a new bot in @botfather and fill the following vales with bottoken and username respectively
-    TG_BOT_TOKEN = "توكن البوت"
+    TG_BOT_TOKEN = "7121027327:AAEVE4wgyK0LKfH62OZ36dPbwknj-nPkvrs"
     # command handler
     COMMAND_HAND_LER = "."
     # sudo enter the id of sudo users userid's in that array
