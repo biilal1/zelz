927799dcf4abe47b2aadae27ec49d281b8f8a377
biilal1 <135733490+biilal1@users.noreply.github.com>
2024-08-14 13:56:33 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -6,11 +6,11 @@ class Development(Config):
     # the name to display in your alive message
     ALIVE_NAME = "BiLaL"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgresql://postgres:oakwox3@localhost:5432/owkwkso"
+    DB_URI = ""
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
-    STRING_SESSION = "1ApWapzMBuwyjrOqC1M_uEykYOuaCul77fEK-VKjcllKSDY70kVI2YV_K0xHlKdc2DWEWsJk51IT43QjjSWq12dHgC7xDiDH_VIZrIKM3pNOVWBnH5xiHvmxASVlz4xlLtGPI28BlZ4m6mzoqt8vGw52LqgZedeVvSNZxEVVtypNrtM9Sv4WGL7GnYJRQgxJ1JtKT_0EZdX91Iyc2Lj5aWYImdqtkCJraulQDJTJYLNSpfukUAnMkEGB5KlMVN8bfsMggvqDUFLkHX9iQDoSL9FfB_SeWzTnRSkNZHZc7TvuSyq_8VokWo9bdR_UGWZgReo3_yR6jpeah4mmjrrnjvaBgqQWhOc4="
+    STRING_SESSION = ""
     # create a new bot in @botfather and fill the following vales with bottoken and username respectively
-    TG_BOT_TOKEN = "7121027327:AAEVE4wgyK0LKfH62OZ36dPbwknj-nPkvrs"
+    TG_BOT_TOKEN = ""
     # command handler
     COMMAND_HAND_LER = "."
     # sudo enter the id of sudo users userid's in that array
